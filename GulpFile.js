e0da051b24609aa6d1ba5eadef55e85ea715116e
JohnataMoreira <165863494+JohnataMoreira@users.noreply.github.com>
2024-09-07 08:06:28 -0300

    finalizando projeto

--- GulpFile.js
+++ GulpFile.js
@@ -1,18 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 
 function compileSass(){
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'expanded'}))
     .pipe(gulp.dest('./build/styles/'));
-}
-
-function scripts(){
-    return gulp.src('./src/scripts/main.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/'));
 }
 
 function compileImages(){
@@ -25,11 +18,4 @@ function watch(){
     gulp.watch('./src/styles/*.scss', compileSass)
 }
 
-exports.default = gulp.parallel(watch, scripts, compileImages);
-
-// exports.watch = watch;
-// exports.default = gulp.parallel(compileSass, scripts, compileImages);
-// exports.watch = function(){
-//     gulp.watch('./src/styles/*.scss', gulp.parallel(compileSass))
-//     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
-// }
+exports.default = gulp.parallel(watch, compileImages);
